d820a33bf8b684a4123c05f82d5a30142ae7ec00
joepichardo <joe@brooklynbedding.com>
2019-06-20 21:55:00 -0700

    Initial Commit

---
+++ IdeaManagement/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  IdeaManagement
+//
+//  Created by Joe Pichardo on 6/20/19.
+//  Copyright © 2019 Joe Pichardo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ IdeaManagementTests/IdeaManagementTests.swift
@@ -0,0 +1,34 @@
+//
+//  IdeaManagementTests.swift
+//  IdeaManagementTests
+//
+//  Created by Joe Pichardo on 6/20/19.
+//  Copyright © 2019 Joe Pichardo. All rights reserved.
+//
+
+import XCTest
+@testable import IdeaManagement
+
+class IdeaManagementTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
