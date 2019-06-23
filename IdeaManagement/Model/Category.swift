da7431d74e04591e2b2d3042c6527fa627a0a2e7
joepichardo <joe@brooklynbedding.com>
2019-06-22 23:05:13 -0700

    add ballot array to category model

--- IdeaManagement/Model/Category.swift
+++ IdeaManagement/Model/Category.swift
@@ -11,15 +11,16 @@ import SwiftUI
 struct Category : Identifiable {
     var id = UUID()
     var name: String
+    var ballots: [Ballot]
 }
 
 #if DEBUG
 let categoryTestData = [
-    Category(name: "App"),
-    Category(name: "Travel"),
-    Category(name: "Business"),
-    Category(name: "Books"),
-    Category(name: "Dinner"),
-    Category(name: "Holiday")
+    Category(name: "App", ballots: ballotTestData),
+    Category(name: "Travel", ballots: ballotTestData),
+    Category(name: "Business", ballots: ballotTestData),
+    Category(name: "Books", ballots: ballotTestData),
+    Category(name: "Dinner", ballots: ballotTestData),
+    Category(name: "Holiday", ballots: ballotTestData),
 ]
 #endif
