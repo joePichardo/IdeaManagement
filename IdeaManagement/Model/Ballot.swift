96cd321b3f4e37b225b695241b99999cbe31857d
joepichardo <joe@brooklynbedding.com>
2019-06-22 23:04:38 -0700

    add idea array to ballot model

--- IdeaManagement/Model/Ballot.swift
+++ IdeaManagement/Model/Ballot.swift
@@ -11,15 +11,16 @@ import SwiftUI
 struct Ballot : Identifiable {
     var id = UUID()
     var name: String
+    var ideas: [Idea]
 }
 
 #if DEBUG
 let ballotTestData = [
-    Ballot(name: "App"),
-    Ballot(name: "Travel"),
-    Ballot(name: "Business"),
-    Ballot(name: "Books"),
-    Ballot(name: "Dinner"),
-    Ballot(name: "Holiday")
+    Ballot(name: "App", ideas: ideaTestData),
+    Ballot(name: "Travel", ideas: ideaTestData),
+    Ballot(name: "Business", ideas: ideaTestData),
+    Ballot(name: "Books", ideas: ideaTestData),
+    Ballot(name: "Dinner", ideas: ideaTestData),
+    Ballot(name: "Holiday", ideas: ideaTestData)
 ]
 #endif
