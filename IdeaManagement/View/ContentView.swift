5e88e6c3030d9ef2b0590843e2ccbaac8b760862
joepichardo <joe@brooklynbedding.com>
2019-07-03 21:30:53 -0700

    remove list view and replace with new design using scroll view and stacks

--- IdeaManagement/View/ContentView.swift
+++ IdeaManagement/View/ContentView.swift
@@ -14,18 +14,32 @@ struct ContentView : View {
     
     var body: some View {
         NavigationView {
-            List {
-                Section {
-                    Button(action: addCategory) {
-                        Text("Add Category")
+            ScrollView {
+                Button(action: {self.addCategory()}) {
+                    HStack {
+                        Text("Add New Category").foregroundColor(.primary)
+                        Spacer()
+                        Image(systemName: "chevron.right.circle.fill")
                     }
+                    .font(.headline)
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(8)
+                        .shadow(radius: 1, y: 1)
+                }.accentColor(Color.green)
+                .padding([.leading, .trailing], 10)
+                
+                ForEach(store.categories.identified(by: \.id)) {
+                    category in
+                    if (self.store.categories.firstIndex(of: category) ?? 1) % 2 == 0 {
+                        CategoryCell(category: category, categories: self.store.categories)
+                    }
+                    
                 }
-                Section {
-                    ForEach(store.categories.identified(by: \.id)) { category in
-                        CategoryCell(category: category)
-                    }.onDelete(perform: delete)
-                    .onMove(perform: move)
-                }
+                .padding([.leading, .trailing], 10)
+                .accentColor(Color.pink)
+                .onDelete(perform: delete)
+                .onMove(perform: move)
             }
             .navigationBarTitle(Text("Categories"))
             .navigationBarItems(trailing: EditButton())
@@ -58,25 +72,67 @@ struct ContentView : View {
 
 struct CategoryCell: View {
     var category: Category
+    var categories: [Category]
     
     var body: some View {
-        NavigationButton(destination: BallotDetail()) {
-            ZStack {
-                Circle()
-                    .foregroundColor(Color.red)
-                Text("100")
-                    .foregroundColor(Color.white)
-                    .font(.body)
-            }.frame(width: 50.0)
-            VStack(alignment: .leading) {
-                Text(category.name)
-                Text("\(category.ballots.count) Lists")
-                    .font(.subheadline)
-                    .foregroundColor(.secondary)
+        HStack {
+            NavigationLink(destination: BallotDetail(store: BallotStore(ballots: category.ballots))) {
+                CategoryCellItem(name: category.name)
+            }
+            IfLet(categories.firstIndex(of: category)) { index in
+                Group {
+                    if self.categories.indices.contains(index + 1) {
+                        NavigationLink(destination: BallotDetail(store: BallotStore(ballots: self.categories[index + 1].ballots))) {
+                            CategoryCellItem(name: self.categories[index + 1].name)
+                        }
+                    } else {
+                        Spacer()
+                            .padding()
+                    }
+                }
             }
         }
     }
 }
+
+struct CategoryCellItem: View {
+    var name: String
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            HStack {
+                Text(name).foregroundColor(.primary)
+                Spacer()
+                Image(systemName: "chevron.right.circle.fill")
+            }
+        }.frame(height: (UIScreen.main.bounds.width / 2) - 60)
+            .font(.headline)
+            .padding()
+            .background(Color.white)
+            .cornerRadius(8)
+            .shadow(radius: 1, y: 1)
+    }
+}
+
+struct IfLet<T, Out: View>: View {
+    let value: T?
+    let produce: (T) -> Out
+    
+    init(_ value: T?, produce: @escaping (T) -> Out) {
+        self.value = value
+        self.produce = produce
+    }
+    
+    var body: some View {
+        Group {
+            if value != nil {
+                produce(value!)
+            }
+        }
+    }
+}
+
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
