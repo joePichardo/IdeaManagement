5bf47ddc97b787a55ca0ac98c127c10a8c1a984a
joepichardo <joe@brooklynbedding.com>
2019-07-17 22:49:53 -0700

    commented out while testing core data

--- IdeaManagement/Store/BallotStore.swift
+++ IdeaManagement/Store/BallotStore.swift
@@ -8,17 +8,34 @@
 
 import SwiftUI
 import Combine
+import CoreData
 
-class BallotStore: BindableObject {
-    var didChange = PassthroughSubject<Void, Never>()
-    
-    var ballots: [Ballot] {
-        didSet {
-            didChange.send(())
-        }
-    }
-    
-    init(ballots: [Ballot] = []) {
-        self.ballots = ballots
-    }
-}
+//class BallotStore: BindableObject {
+//    var didChange = PassthroughSubject<Void, Never>()
+//    
+//    private let appDelegate = UIApplication.shared.delegate as! AppDelegate
+//    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext  
+//    private var fetchedRC: NSFetchedResultsController<List>!
+//    
+//    var ballots: [List] {
+//        didSet {
+//            didChange.send(())
+//        }
+//    }
+//    
+//    init(ballots: [List] = []) {
+//        self.ballots = ballots
+//    }
+//}
+
+#if DEBUG
+//let ballotTestData = [
+//    List(name: "List #1", ideas: ideaTestData),
+//    List(name: "List #2", ideas: ideaTestData),
+//    List(name: "List #3", ideas: ideaTestData),
+//    List(name: "List #4", ideas: ideaTestData),
+//    List(name: "List #5", ideas: ideaTestData),
+//    List(name: "List #6", ideas: ideaTestData),
+//    List(
+//]
+#endif
--- IdeaManagement/View/BallotDetail.swift
+++ IdeaManagement/View/BallotDetail.swift
@@ -10,78 +10,79 @@ import SwiftUI
 
 struct BallotDetail : View {
     
-    @ObjectBinding var store = BallotStore()
+//    @ObjectBinding var store = BallotStore()
     
     var body: some View {
-        ScrollView {
-            ForEach(store.ballots.identified(by: \.id)) {
-                ballot in
-                NavigationLink(destination: Text(ballot.name)) {
-                    if ballot.id == self.store.ballots[0].id {
-                        HStack {
-                            Text("Add New List").foregroundColor(.primary)
-                            Image(systemName: "checkmark.seal.fill").foregroundColor(.green)
-                            Spacer()
-                            Image(systemName: "chevron.right.circle.fill")
-                        }
-                        .font(.system(size: 24, design: .rounded))
-                            .padding()
-                            .background(Color.white)
-                            .cornerRadius(8)
-                            .shadow(radius: 1, y: 1)
-                        HStack {
-                            Text(ballot.name).foregroundColor(.primary)
-                            Image(systemName: "checkmark.seal.fill").foregroundColor(.green)
-                            Spacer()
-                            Image(systemName: "chevron.right.circle.fill")
-                        }
-                        .font(.system(size: 24, design: .rounded))
-                            .padding()
-                            .background(Color.white)
-                            .cornerRadius(8)
-                            .shadow(radius: 1, y: 1)
-                    } else {
-                        HStack {
-                            Text(ballot.name).foregroundColor(.primary)
-                            Image(systemName: "checkmark.seal.fill").foregroundColor(.green)
-                            Spacer()
-                            Image(systemName: "chevron.right.circle.fill")
-                        }
-                        .font(.system(size: 24, design: .rounded))
-                            .padding()
-                            .background(Color.white)
-                            .cornerRadius(8)
-                            .shadow(radius: 1, y: 1)
-                    }
-                }
-            }
-            .padding([.leading, .trailing], 15)
-                .accentColor(Color.pink)
-        }.navigationBarTitle(Text("Lists"))
-            .navigationBarItems(trailing: EditButton())
-            .listStyle(.grouped)
+        Text("hi")
+//        ScrollView {
+//            ForEach(store.ballots.identified(by: \.id)) {
+//                ballot in
+//                NavigationLink(destination: Text(ballot.name)) {
+//                    if ballot.id == self.store.ballots[0].id {
+//                        HStack {
+//                            Text("Add New List").foregroundColor(.primary)
+//                            Image(systemName: "checkmark.seal.fill").foregroundColor(.green)
+//                            Spacer()
+//                            Image(systemName: "chevron.right.circle.fill")
+//                        }
+//                        .font(.system(size: 24, design: .rounded))
+//                            .padding()
+//                            .background(Color.white)
+//                            .cornerRadius(8)
+//                            .shadow(radius: 2, y: 0)
+//                        HStack {
+//                            Text(ballot.name).foregroundColor(.primary)
+//                            Image(systemName: "checkmark.seal.fill").foregroundColor(.green)
+//                            Spacer()
+//                            Image(systemName: "chevron.right.circle.fill")
+//                        }
+//                        .font(.system(size: 24, design: .rounded))
+//                            .padding()
+//                            .background(Color.white)
+//                            .cornerRadius(8)
+//                            .shadow(radius: 2, y: 0)
+//                    } else {
+//                        HStack {
+//                            Text(ballot.name).foregroundColor(.primary)
+//                            Image(systemName: "checkmark.seal.fill").foregroundColor(.green)
+//                            Spacer()
+//                            Image(systemName: "chevron.right.circle.fill")
+//                        }
+//                        .font(.system(size: 24, design: .rounded))
+//                            .padding()
+//                            .background(Color.white)
+//                            .cornerRadius(8)
+//                            .shadow(radius: 2, y: 0)
+//                    }
+//                }
+//            }
+//            .padding([.leading, .trailing], 15)
+//                .accentColor(Color.pink)
+//        }.navigationBarTitle(Text("Lists"))
+//            .navigationBarItems(trailing: EditButton())
+//            .listStyle(.grouped)
     }
     
     func addList() {
-        store.ballots.insert(Ballot(name: "List #", ideas: ideaTestData), at: 0)
+//        store.ballots.insert(List(context: <#T##NSManagedObjectContext#>), at: 0)
     }
     
     func delete(at offsets: IndexSet) {
-        if let first = offsets.first {
-            store.ballots.remove(at: first)
-        }
+//        if let first = offsets.first {
+//            store.ballots.remove(at: first)
+//        }
     }
     
     func move(from source: IndexSet, to destination: Int) {
         
-        // sort the indexes low to high
-        let reversedSource = source.sorted()
-        
-        // then loop from the back to avoid reordering problems
-        for index in reversedSource.reversed() {
-            // for each item, remove it and insert it at the destination
-            store.ballots.insert(store.ballots.remove(at: index), at: destination)
-        }
+//        // sort the indexes low to high
+//        let reversedSource = source.sorted()
+//
+//        // then loop from the back to avoid reordering problems
+//        for index in reversedSource.reversed() {
+//            // for each item, remove it and insert it at the destination
+//            store.ballots.insert(store.ballots.remove(at: index), at: destination)
+//        }
     }
 }
 
@@ -111,9 +112,10 @@ struct BallotCell: View {
 #if DEBUG
 struct BallotDetail_Previews : PreviewProvider {
     static var previews: some View {
-        NavigationView {
-            BallotDetail(store: BallotStore(ballots: ballotTestData))
-        }
+        Text("hi")
+//        NavigationView {
+//            BallotDetail(store: BallotStore(ballots: ballotTestData))
+//        }
     }
 }
 #endif
